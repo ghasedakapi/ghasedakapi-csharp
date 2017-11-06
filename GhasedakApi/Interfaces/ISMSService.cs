86abf2f5c97c0c9d80ff2bf57736ba83d96f1743
saeid maroufi <saeidmaroufi66@gmail.com>
2017-11-06 16:31:15 +0330

    get account info

---
+++ GhasedakApi/Interfaces/ISMSService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static GhasedakApi.Models.Results;
+
+namespace GhasedakApi.Interfaces
+{
+    public interface ISMSService
+    {
+        ApiResult Send(string message, string linenumber, string receptor);
+        ApiResult Send(string message, string linenumber, string receptor, DateTime senddate);
+
+        ApiResult Send(string message, string linenumber, string[] receptor);
+        ApiResult Send(string message, string linenumber, string[] receptor, DateTime senddate);
+
+        ApiResult Send(string[] message, string[] linenumber, string[] receptor);
+        ApiResult Send(string[] message, string[] linenumber, string[] receptor, DateTime[] senddate);
+        ApiResult Verify(int type, string template, string[] receptor, string param1, string param2, string param3);
+        ApiResult GetStatus(string messageId);
+
+    }
+}
---
+++ GhasedakApi/api.cs
@@ -0,0 +1,169 @@
+﻿using GhasedakApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+using static GhasedakApi.Models.Results;
+using GhasedakApi.Models;
+
+namespace GhasedakApi
+{
+    public class Api : ISMSService, IAccountService, IVoiceService
+    {
+        private readonly string _apikey;
+        private static readonly JavaScriptSerializer _JavaScriptSerializer = new JavaScriptSerializer();
+        public Api(string apikey)
+        {
+            _apikey = apikey;
+        }
+
+        #region sms
+        public ApiResult SendSMS(string message, string linenumber, string receptor)
+        {
+            var url = "api/v1/sms/send/simple";
+            var param = new Dictionary<string, object>
+            {
+                {"apikey", _apikey},
+                {"linenumber", linenumber},
+                {"receptor",receptor },
+                {"message", System.Web.HttpUtility.UrlEncodeUnicode(message)
+            }
+        };
+            return MakeRequest(url, param);
+        }
+        public ApiResult SendSMS(string message, string linenumber, string[] receptor)
+        {
+            var url = "api/v1/sms/send/bulk2";
+            var param = new Dictionary<string, object>
+             {
+               {"apikey", _apikey},
+               {"linenumber", linenumber},
+               {"receptor",string.Join(",",receptor) },
+               {"message", System.Web.HttpUtility.UrlEncodeUnicode(message)},
+        };
+            return MakeRequest(url, param);
+        }
+        public ApiResult SendSMS(string[] message, string[] linenumber, string[] receptor)
+        {
+            var url = "api/v1/sms/send/bulk";
+            var msg = new System.Text.StringBuilder();
+            foreach (var item in message)
+            {
+                msg.Append(System.Web.HttpUtility.UrlEncodeUnicode(item)).Append(",");
+            }
+            var param = new Dictionary<string, object>
+               {
+                  {"apikey", _apikey},
+                  {"linenumber", string.Join(",",linenumber)},
+                  {"receptor",string.Join(",",receptor) },
+                  {"message", msg},
+                };
+            return MakeRequest(url, param);
+        }
+        public ApiResult SendSMS(string message, string linenumber, string receptor, DateTime senddate)
+        {
+            var url = "api/v1/sms/send/simple";
+            var param = new Dictionary<string, object>
+              {
+                {"apikey", _apikey},
+                {"linenumber", linenumber},
+                {"receptor",receptor },
+                {"message", System.Web.HttpUtility.UrlEncodeUnicode(message) },
+                {"senddate",Utilities.Date_Time.DatetimeToUnixTimeStamp(senddate) }
+        };
+            return MakeRequest(url, param);
+        }
+        public ApiResult SendSMS(string message, string linenumber, string[] receptor, DateTime senddate)
+        {
+            var url = "api/v1/sms/send/bulk2";
+            var param = new Dictionary<string, object>
+             {
+               {"apikey", _apikey},
+               {"linenumber", linenumber},
+               {"receptor",string.Join(",",receptor) },
+               {"message", System.Web.HttpUtility.UrlEncodeUnicode(message)},
+               {"senddate",Utilities.Date_Time.DatetimeToUnixTimeStamp(senddate)
+             }
+        };
+            return MakeRequest(url, param);
+        }
+        public ApiResult SendSMS(string[] message, string[] linenumber, string[] receptor, DateTime[] senddate)
+        {
+            var url = "api/v1/sms/send/bulk";
+            var msg = new System.Text.StringBuilder();
+            var date = new System.Text.StringBuilder();
+            for (int i = 0; i < message.Length; i++)
+            {
+                msg.Append(System.Web.HttpUtility.UrlEncodeUnicode(message[i])).Append(",");
+                date.Append(Utilities.Date_Time.DatetimeToUnixTimeStamp(senddate[i])).Append(",");
+            }
+            var param = new Dictionary<string, object>
+               {
+                  {"apikey", _apikey},
+                  {"linenumber", string.Join(",",linenumber)},
+                  {"receptor",string.Join(",",receptor) },
+                  {"message", msg},
+                  {"senddate", date}
+                };
+            return MakeRequest(url, param);
+        }
+        public ApiResult Verify(int type, string template, string[] receptor, string param1, string param2, string param3)
+        {
+            var url = "api/v1/sms/send/verify";
+            var param = new Dictionary<string, object>
+        {
+            {"apikey", _apikey},
+            {"type", type},
+            {"receptor",string.Join(",",receptor) },
+            {"param1", param1},
+            {"param2", param2},
+            {"param3", param3},
+            {"template", template}
+        };
+            var response = Client.ApiClient.Execute(url, param);
+            return _JavaScriptSerializer.Deserialize<ApiResult>(response);
+        }
+        public ApiResult GetStatus(string messageId)
+        {
+            var url = "api/v1/sms/status";
+            var param = new Dictionary<string, object>
+        {
+            {"apikey", _apikey},
+            {"messageid", messageId},
+        };
+            var response = Client.ApiClient.Execute(url, param);
+            return _JavaScriptSerializer.Deserialize<ApiResult>(response);
+        }
+        #endregion
+        public ApiResult AccountInfo()
+        {
+            var url = "api/v1/account/status";
+            var param = new Dictionary<string, object>
+        {
+            {"apikey", _apikey}
+        };
+            var response = Client.ApiClient.Execute(url, param);
+            return _JavaScriptSerializer.Deserialize<ApiResult>(response);
+        }
+
+        public ApiResult SendVoice(string message, string[] receptor, string senddate)
+        {
+            var url = "api/v1/voice/send";
+            var param = new Dictionary<string, object>
+                {
+                 {"apikey", _apikey},
+                 {"message", message},
+                 {"senddate", senddate},
+                };
+            var response = Client.ApiClient.Execute(url, param);
+            return _JavaScriptSerializer.Deserialize<ApiResult>(response);
+        }
+
+        private ApiResult MakeRequest(string url, Dictionary<string, object> param)
+        {
+            return _JavaScriptSerializer.Deserialize<ApiResult>(Client.ApiClient.Execute(url, param));
+        }
+    }
+}
